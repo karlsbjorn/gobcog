5afaf2aecd08df65774ce0628e8c4111549dc9e3
Karlo Prikratki <karlo@karloprikratki.com>
2024-05-04 08:34:55 +0200

    Update adventure.py

--- adventure/adventure.py
+++ adventure/adventure.py
@@ -2675,9 +2675,9 @@ class Adventure(
                 continue
             userxp = int(xp + (xp * 0.5 * c.rebirths) + max((xp * 0.1 * min(250, c._int / 10)), 0))
             usercp = int(cp + max((cp * 0.1 * min(1000, (c._luck + c._att) / 10)), 0))
-            userxp = int(userxp * (c.gear_set_bonus.get("xpmult", 1) + daymult + session_bonus))
+            userxp = int((userxp * (c.gear_set_bonus.get("xpmult", 1) + daymult + session_bonus)) * 5)
             usercp = int(usercp * (c.gear_set_bonus.get("cpmult", 1) + daymult))
-            newxp += userxp * 5
+            newxp += userxp
             newcp += usercp
             roll = random.randint(1, 5)
             if c.heroclass.get("pet", {}).get("bonuses", {}).get("always", False):
